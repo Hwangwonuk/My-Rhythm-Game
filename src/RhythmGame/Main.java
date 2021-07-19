e2dc0175a7d8de02d822aecce73385f47a4519f5
Hwangwonuk <hok0173@naver.com>
2021-07-19 20:27:29 +0900

    Create a game screen

--- src/RhythmGame/Main.java
+++ src/RhythmGame/Main.java
@@ -2,8 +2,12 @@ package RhythmGame;
 
 public class Main {
 
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	// 상수는 모두 대문자를 이용하여 만든다.
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new MyRhythmGame();
 
 	}
 
